155e6f5db297b9dd504c6f24f564b460168aea92
Neel Niranjan <niranjanneel009@gmail.com>
2026-03-03 11:21:36 +0530

    implemented redux store for user

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
 
+import { updateName } from "./userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 function CreateUser() {
   const [username, setUsername] = useState("");
 
+  // we have to use useDispatch to dispatch any action inside the redux store
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // when the form is submit then only we update the username at the redux store
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate("/menu");
+    setUsername("");
   }
 
   return (
